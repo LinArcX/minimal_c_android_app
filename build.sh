2f058ef9880c66e17fe7bb9360a2f7718317a89e
linarcx <linarcx@gmail.com>
2025-04-13 19:45:46 +0200

    updated

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-echo "building for aarch64"
+echo -e "\nbuilding for aarch64"
 ./build/build_aarch64.sh
 
-echo "building for armeabi-v7a"
+echo -e "\nbuilding for armeabi-v7a"
 ./build/build_armeabi_v7a.sh
 
-echo "building for x86_64"
+echo -e "\nbuilding for x86_64"
 ./build/build_x86_64.sh
